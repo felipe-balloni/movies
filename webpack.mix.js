2d6258b0901fc2a4b4aa6c23f0dcdb746a5d0fc9
Felipe Balloni Ferreira <felipe.balloni@hotmail.com>
2020-03-30 14:18:13 -0300

    Initial commit

---
+++ webpack.mix.js
@@ -0,0 +1,32 @@
+const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss')
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    // .options({
+    //     processCssUrls: false,
+    //     postCss: [tailwindcss('tailwind.config.js')],
+    // })
+    .postCss('resources/css/main.css', 'public/css', [
+        require('tailwindcss'),
+    ])
+
+if (mix.inProduction()) {
+    mix.version();
+}
+
+mix.browserSync({
+    proxy: 'movies.app'
+})
+
